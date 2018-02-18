8f3b047bae55ffa1ef5b783fe4dad314cc70fe95
Matthew Broman <broman334@tamu.edu>
2018-02-18 14:36:32 -0600

    part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,14 +13,19 @@ class MoviesController < ApplicationController
 
   def index
     @all_ratings = ['G', 'PG', 'PG-13', 'R']
-    @sortby = params[:sort]
-    if (params[:ratings] != nil)
-	  @movies = Movie.where(rating: params[:ratings].keys).order(params[:sort])
-	  @checked = params[:ratings].keys
-    else
-      @movies = Movie.order(params[:sort])
-      @checked = @all_ratings
+    unless (params[:ratings] == nil)
+      session[:ratings] = params[:ratings]
+    end 
+    unless (params[:sort] == nil)
+      session[:sort] = params[:sort]
     end
+    if (session[:ratings] == nil)
+      session[:ratings] = Hash[@all_ratings.map{ |x| [x, 1]}]
+    end
+	@movies = Movie.where(rating: session[:ratings].keys).order(session[:sort])
+	@sortby = session[:sort]
+	@checked = session[:ratings].keys
+	  
   end
   
   
@@ -31,6 +36,7 @@ class MoviesController < ApplicationController
   def create
     @movie = Movie.create!(movie_params)
     flash[:notice] = "#{@movie.title} was successfully created."
+    flash.keep
     redirect_to movies_path
   end
 
@@ -42,6 +48,7 @@ class MoviesController < ApplicationController
     @movie = Movie.find params[:id]
     @movie.update_attributes!(movie_params)
     flash[:notice] = "#{@movie.title} was successfully updated."
+    flash.keep
     redirect_to movie_path(@movie)
   end
 
@@ -49,6 +56,7 @@ class MoviesController < ApplicationController
     @movie = Movie.find(params[:id])
     @movie.destroy
     flash[:notice] = "Movie '#{@movie.title}' deleted."
+    flash.keep
     redirect_to movies_path
   end
 
